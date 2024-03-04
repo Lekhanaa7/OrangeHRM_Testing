d1ecb1fd1c5921ba29799c61757baf408b4a259d
Lekhanaa7 <158190793+Lekhanaa7@users.noreply.github.com>
2024-03-04 17:57:20 +0530

    added testng

---
+++ orangehrm/src/main/java/com/testing/orangehrm/OrangehrmApplication.java
@@ -0,0 +1,13 @@
+package com.testing.orangehrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangehrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangehrmApplication.class, args);
+	}
+
+}
